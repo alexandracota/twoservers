9a24540232be6d2912382ceb313f30fd9e15641a
Alex Cota <cota.alexandra14@gmail.com>
2017-07-24 19:34:02 -0700

    second commit

--- logic.js
+++ logic.js
@@ -3,22 +3,24 @@ var http = require("http");
 var PORTgood = 7000;
 var PORTbad = 7500;
 
-function handleRequestGood(request, response) {
-	reponse.end("It works!! Path Hit: " + request.url);
+if (http === PORTgood) {
+	function handleRequestGood(request, response) {
+		reponse.end("It works!! Path Hit: " + request.url);
 
-	var server = http.createServer(handleRequest);
+		var server = http.createServer(handleRequest);
 
-	server.listen(PORTgood, function() {
-		console.log("You stink.");
-	})
-};
+		server.listen(PORTgood, function() {
+			console.log("You're so talented!");
+		})
+	}
+} else if (http === PORTbad) {
+	function handleRequestBad(request, response) {
+		reponse.end("It works!! Path Hit: " + request.url);
 
-function handleRequestBad(request, response) {
-	reponse.end("It works!! Path Hit: " + request.url);
+		var server = http.createServer(handleRequest);
 
-	var server = http.createServer(handleRequest);
-
-	server.listen(PORTbad, function() {
-		console.log("You're so talented!");
-	})
+		server.listen(PORTbad, function() {
+			console.log("You stink...");
+		})
+	};
 };
